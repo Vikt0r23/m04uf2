a5979471af63853ba113f1a85b653dca90a4133f
Victor Lopez Garcia <victor.lopez.garcia@enti.cat>
2024-04-22 11:19:07 +0200

    items

--- characters_bs4.py
+++ characters_bs4.py
@@ -7,8 +7,50 @@ file = open('characters.facx', 'r')
 
 soup = BeautifulSoup(file, 'xml')
 
+characters = soup.find_all("character")
+
+file.close()
+
+
+title = "BIENVENIDOS A FARYADVENTURES"
+print(title)
+print("="*len(title))
+
+print("\nQuien quieres ser?\n")
+
+for character in characters:
+	print(f"{character['id']}\t{character.find['name'].text}")
+
+encontrado = False
+
+while not encontrado:
+id = input("\nIntroduce un numero: ")
+
+file = open('characters.facx', 'r')
+	#etiqueta = character.find('id')
+	#print(etiqueta['value'])
+soup = BeautifulSoup(file, 'xml')
+
+file.close()
 
 
 
-print(soup.prettify())
+character = soup.find('character', {'id': choose})
+
+if not character:
+	print('ERROR: ID NO ENCONTRADO')
+	exit()
+else:
+	encontrado = True
+
+print("\nEl personaje escogido es:\n")
+print(f"\tNombre: {character.find('name').text}")
+print(f"\tEdad: {character.find('age').text}")
+print(f"\tGenero: {character.find('gender')['value']}")
+print(f"\tNivel: {character.find('level')['value']}")
+
+
+
+
+
 
